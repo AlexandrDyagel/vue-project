55ad19ea4c378a6c3f36b86b9a43559cb05afd81
Alexander Dyagel <iamdruid@mail.ru>
2024-07-24 21:55:51 +0300

    Add routing with BottomMenu

--- src/components/BottomMenu.vue
+++ src/components/BottomMenu.vue
@@ -6,16 +6,16 @@ import MenuItem from './BottomMenuItem.vue';
   <div class="fixed bottom-0 w-full bg-black">
     <ul class="flex justify-around">
       <li class="w-1/4">
-        <MenuItem image-url="src/assets/home-24dp.svg" title="Биржа"/>
+        <MenuItem image-url="src/assets/home-24dp.svg" title="Биржа" route="/"/>
       </li>
       <li class="w-1/4">
-        <MenuItem  image-url="src/assets/add_circle_24dp.svg" title="Добавить" />
+        <MenuItem  image-url="src/assets/add_circle_24dp.svg" title="Добавить" route="/add" />
       </li>
       <li class="w-1/4">
-        <MenuItem  image-url="src/assets/cached_24dp.svg" title="История" />
+        <MenuItem  image-url="src/assets/cached_24dp.svg" title="История" route="/history" />
       </li>
       <li class="w-1/4">
-        <MenuItem  image-url="src/assets/person_24dp.svg" title="Профиль" />
+        <MenuItem  image-url="src/assets/person_24dp.svg" title="Профиль" route="/user" />
       </li>
     </ul>
   </div>
--- src/components/BottomMenuItem.vue
+++ src/components/BottomMenuItem.vue
@@ -2,14 +2,17 @@
 
 defineProps({
   imageUrl: String,
-  title: String
+  title: String,
+  route: String
 })
 
 </script>
 
 <template>
-  <div class="text-white m-4 p-4 h-3/4 w-auto border border-amber-500 cursor-pointer transition hover:scale-105 hover:border-amber-700 rounded-3xl grid grid-flow-row place-items-center gap-1.5">
-    <img :src="imageUrl" :alt="title" />
-    <p>{{ title }}</p>
-  </div>
+  <RouterLink :to="route">
+    <div class="text-white m-4 p-4 h-3/4 w-auto border border-amber-500 cursor-pointer transition hover:scale-105 hover:border-amber-700 rounded-3xl grid grid-flow-row place-items-center gap-1.5">
+      <img :src="imageUrl" :alt="title" />
+      <p>{{ title }}</p>
+    </div>
+  </RouterLink>
 </template>
